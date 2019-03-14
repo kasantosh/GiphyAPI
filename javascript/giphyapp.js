bc2db877fef581deddf212f792734fd74bc931fe
Santosh Kumar <kasantosh@yahoo.com>
2019-03-14 10:21:53 -0400

    completed assignment with image pause/play still incomplete

--- javascript/giphyapp.js
+++ javascript/giphyapp.js
@@ -1,7 +1,27 @@
 $(document).ready(function() {
-  var animalArray = ["Dog", "Tiger", "Lion", "Cat", "Skunk"];
+  var animalArray = [
+    "Dog",
+    "Tiger",
+    "Lion",
+    "Cat",
+    "Skunk",
+    "Camel",
+    "Giraffe",
+    "Penguin",
+    "Ostrich",
+    "Eagle",
+    "Raccoon",
+    "Bear",
+    "Coyote",
+    "cougar",
+    "snake"
+  ];
   var i = 0;
-  var j = 5;
+  var j = animalArray.length;
+  var newDiv;
+  var newImg;
+  var gifImage;
+  var gifImageStill;
 
   $.each(animalArray, function() {
     var newBtn1 = $("<button>");
@@ -19,29 +39,60 @@ $(document).ready(function() {
     var searchTag = $("#searchQuery")
       .val()
       .trim();
-    animalArray.push(searchTag);
-    $(newBtn).text(searchTag);
-    $(newBtn).attr("id", "id-" + j);
-    $(newBtn).addClass("btnClass");
-    j++;
-    $("#btnsdiv").append(newBtn);
-    console.log(animalArray);
+    if (searchTag === "") {
+      $("#errorDisplay").text("Please enter a valid animal name");
+    } else {
+      animalArray.push(searchTag);
+      $(newBtn).text(searchTag);
+      $(newBtn).attr("id", "id-" + j);
+      $(newBtn).addClass("btnClass");
+      j++;
+      $("#btnsdiv").append(newBtn);
+      console.log(animalArray);
+    }
   }); //--------------------------end of searchButton click function
 
-  $("#btnsdiv :button").on("click", function() {
-    var animalQuery = this.innerHTML;
+  $(document).on("click", ".btnClass", function() {
+    var animalQuery = $(this).text();
     console.log(animalQuery);
     var queryURL =
       "http://api.giphy.com/v1/gifs/search?q=" +
       animalQuery +
       "&api_key=LUVAX7z5WrsSlE8rbSgpuLSkE3KeteFM&limit=10&rating=g";
-    //var queryURL = animalArray[this];
     console.log(queryURL);
     $.ajax({
       url: queryURL,
       method: "GET"
     }).then(function(response) {
       console.log(response);
-    });
-  }); //---------------------------------end of button:this
+      $("#populate").empty();
+      for (var i = 0; i < 10; i++) {
+        newDiv = $("<div class = 'displayAnimal'>");
+        newImg = $("<img>");
+        gifImage = response.data[i].images.fixed_width.url;
+        gifImageStill = response.data[i].images.fixed_width_still.url;
+        newImg.attr("src", gifImage);
+        var title = $('<span class = "title">');
+        title.text(response.data[i].title);
+        title.addClass("title");
+        var rating = response.data[i].rating;
+        newDiv
+          .append("Title: ", title)
+          .append("<br>")
+          .append(newImg)
+          .append("<br>")
+          .append("Rating: ", rating);
+
+        $("#populate").append(newDiv);
+      }
+    }); //--------------------------- end of Ajax
+  }); //---------------------------------end of document.on(Click)
+  $(document).on("click", "img", function() {
+    var src = $("#displayAnimal img").attr("src");
+    if (src == gifImage) {
+      $("#displayAnimal img").attr("src", gifImageStill);
+    } else {
+      $("#displayAmimal img").attr("src", gifImage);
+    }
+  });
 }); //--------------------------end of document.ready function
